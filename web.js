2bb81128695d4c3c6fe95c74f83fcc36757a15c2
Alexandra Mendes <asf.mendes@gmail.com>
2013-07-10 23:12:11 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	response.send(Buffer.String(fs.readFileSync('index.html')));
+	response.send(fs.readFileSync('index.html')i.toString());
 // response.send('Hello World 2!');
 });
 
